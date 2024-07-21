7ba74c5eb525b7480f64e643cf19d31db6ad5393
KARIMANSHAWKI <kariman0800@gmail.com>
2024-07-21 23:44:00 +0300

    upload project with docker env

--- app/resources/lang/ar/message.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-return [
-    'register-success' => 'تم التسجيل بنجاح.',
-    'login-success' => 'تم تسجيل الدخول بنجاح.',
-    'invalid_user' => 'عذراً، بيانات التسجيل غير صالحة.',
-    'interval-submitted-successfully' => 'تم إرسال فترة الكتاب بنجاح'
-];
